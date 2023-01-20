9b31a1d3efd5c5bb4703a10580f6201264b527e6
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-19 19:37:33 -0800

    add a message to pigLatin() which will add way and print to the console any given word starting with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,13 @@ function pigLatin(arg) {
   let x = arg.split('')
   for(let i = 0; i < x.length ; i++) {
       if (x[0] === 'a' || x[0] === 'e' || x[0] === 'i' || x[0] === 'o' || x[0] === 'u') {
-          console.log('starts with vowel')
+        x.join('');
+          console.log(x.join('') + 'way');
           break;
       } else {
           if (x[i] === 'a' || x[i] === 'e' || x[i] === 'i' || x[i] === 'o' || x[i] === 'u') {
               console.log('starts with consonant');
-              console.log("hit a vowel at index:" + ' ' + (i))
+              console.log("number of consonants:" + ' ' + (i))
           break;
           }
       }
